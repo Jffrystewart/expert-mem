9d2714f575a8d594df2b01481f410301cb50fcf3
Mcbeeff <jffrystwrt@gmail.com>
2020-05-07 07:25:17 -0700

    clarification

--- binaryshift.cpp
+++ binaryshift.cpp
@@ -1,23 +1,23 @@
 #include<iostream>
 #include<cstring>
 
-// shift each byte from a binary string, N number of bytes to either the left or the right.
+// shift each bit from a binary string, N number of bits to either the left or the right.
 
 using namespace std;
 
 void shiftStringR(char strMsg[]);
-//called by functiong shiftString 4. Sets tmp equal to byte indexed at length of strMsg, then loops through each byte starti
-//begining at index i= length of strMsg -1 and setting it equal to index i-1 effectively moving each byte to the right, ignoring NULL terminator.
+//called by functiong shiftString 4. Sets tmp equal to bit indexed at length of strMsg, then loops through each bit starti
+//begining at index i= length of strMsg -1 and setting it equal to index i-1 effectively moving each bit to the right, ignoring NULL terminator.
 
 void shiftString4(char strMsg[], int &num);
-//while loop. so that we can shift each byte N number of times depending on user input.
+//while loop. so that we can shift each bit N number of times depending on user input.
 
 void shiftString6(char strMsg[], int &num);
-//while loop does the same as shiftString 4 except calls function shiftString6 intead. This is used for moving byte to the left.
+//while loop does the same as shiftString 4 except calls function shiftString6 intead. This is used for moving bit to the left.
 
 void shiftStringL(char strMsg[]);
 //called by function shiftString6. Sets tmp equal to byte at index 0 of strMsg, then loops through each byte starting at index i and setting
-//them equal to the next value at index i+1 effectively moving each byte to the left, ignoring NULL terminator.
+//them equal to the next value at index i+1 effectively moving each bit to the left, ignoring NULL terminator.
 
 int main(){
   char str[] = "1010010";
@@ -25,7 +25,7 @@ int main(){
   int direction;
 
 
-  cout << "How many bytes do you want to shift" << endl;
+  cout << "How many bits do you want to shift" << endl;
   cin >> numberOfBits;
   cout << "Shift to the right or left" << endl;
   cout << "1. Right\n";
